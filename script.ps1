f9a1b058124ee1179476414a37f1e1e49f6c1a49
edwin kullu <edwinkullu94@gmail.com>
2023-06-05 14:38:10 +0530

    update new feature

--- script.ps1
+++ script.ps1
@@ -4,6 +4,7 @@ git config --global user.name "edwin kullu"
 git config --global user.email "edwinkullu94@gmail.com"
 git add .
 git commit -m "update new feature"
-git push origin main
-git add remote origin https://github.com/edwinkullu/WebApp.git
 
+git remote add origin https://github.com/edwinkullu/WebApp.git
+
+git push --force origin main
