bb57965b8469bb6fa26b0634173dbc61fd6106c1
edwin kullu <edwinkullu94@gmail.com>
2023-06-05 14:31:46 +0530

    update new feature

--- script.ps1
+++ script.ps1
@@ -1,26 +1,8 @@
-# Set the repository information
-$branchName = "main"
-$commitMessage = "Update new files"
-$filePath = "C:\WebApp"
 
-# Set the authentication token (optional if the repository is public)
-$authToken = ""
 
-# Set the Git remote URL
-$remoteUrl = "https://github.com/edwinkullu/WebApp.git"
-
-# Configure Git user
 git config --global user.name "edwin kullu"
 git config --global user.email "edwinkullu94@gmail.com"
+git add .
+git commit -m "update new feature"
+git push origin main
 
-# Stage the changes
-git add $filePath
-
-# Commit the changes
-git commit -m $commitMessage
-
-# Push the changes to the remote repository
-if ($authToken) {
-    $remoteUrl = $remoteUrl -replace "^https://", "https://$($authToken)@"
-}
-git push $remoteUrl $branchName
