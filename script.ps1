a0fde4bf1728fac08c22d70589834a3b1a6e4f9f
edwin kullu <edwinkullu94@gmail.com>
2023-06-05 14:40:32 +0530

    update new file

--- script.ps1
+++ script.ps1
@@ -1,10 +1,15 @@
 
 
+git init
+
 git config --global user.name "edwin kullu"
+
 git config --global user.email "edwinkullu94@gmail.com"
+
 git add .
-git commit -m "update new feature"
+
+git commit -m "update new file"
 
 git remote add origin https://github.com/edwinkullu/WebApp.git
 
-git push --force origin main
+git push  origin main
