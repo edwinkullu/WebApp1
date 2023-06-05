115f63e971eb60e1fa7af0a4c78d810bdb915a9b
edwinkullu <edwinkullu94@gmail.com>
2023-06-05 13:17:30 +0530

    .Net web application

---
+++ script.ps1
@@ -0,0 +1,24 @@
+
+
+git config --global user.name "edwinkullu"
+git config --global user.email "edwinkullu94@gmail.com"
+
+$repoPath = "C:\WebApp"
+$commitMessage = ".Net web application "
+$branchName = "main"  # Replace with your desired branch name
+
+
+
+
+# Change directory to the repository path
+Set-Location -Path $repoPath
+
+# Stage all changes
+git add .
+
+# Commit the changes with the specified commit message
+git commit -m $commitMessage
+
+# Push the changes to the remote repository
+git remote add origin https://github.com/edwinkullu/WebApp.git
+git push origin $branchName
